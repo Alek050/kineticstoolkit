8acf84938fcc4b1cbb40397bfa2bc43ef8592870
Alexander Oonk <alexanderoonk26@gmail.com>
2024-03-12 18:19:24 +0100

    Added test

--- tests/test_files.py
+++ tests/test_files.py
@@ -277,6 +277,16 @@ def test_read_c3d_event_name_format():
     )
     assert test["Points"].events[0].name == "Foot Strike"
 
+def test_read_c3d_rotations():
+    """Test read_c3d with a file containing only rotation matrices."""
+    c3d = ktk.read_c3d(ktk.doc.download("C3DRotationExample.c3d"), convert_point_unit=True)
+
+    assert len(c3d["Points"].data) == 0
+    assert "Analogs" not in c3d.keys()
+    for key in c3d["Rotations"].data.keys():
+        assert c3d["Rotations"].data[key].shape == (340, 4, 4)
+    assert c3d["Rotations"].time_info["Unit"] == "s"
+    np.testing.assert_allclose(c3d["Rotations"].time, np.arange(0, 340) / 85)
 
 def test_read_write_c3d():
     """
